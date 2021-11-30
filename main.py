bf537b514e68c8250159c6eabfc2033ac9f30b5f
DymitriChabert <dymitri.chabert@yahoo.fr>
2021-11-30 15:17:24 +0100

    supréssion BDd.py

--- main.py
+++ main.py
@@ -14,3 +14,61 @@ app = Flask(__name__)
 def index():
     
     return render_template('index.html')
+
+
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+# https://sites.uclouvain.be/P2SINF/flask/tutorial/database.html
+
+# Connexion BDD
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+# Deconnexion BDD
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()  
+
+# Création des tables
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+def create_app():
+    app = ...
+    # existing code omitted
+
+    from . import db
+    db.init_app(app)
+
+    return app
