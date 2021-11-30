07c398394e3d55f40091c80544646ef0f4cf0198
DymitriChabert <dymitri.chabert@yahoo.fr>
2021-11-30 14:39:27 +0100

    Data base
    Ajout bdd.py  remplissage bdd.sql

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sqlite3
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
 from flask import Flask,render_template
 
 app = Flask(__name__)
@@ -5,6 +9,6 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
     
     return render_template('index.html')
